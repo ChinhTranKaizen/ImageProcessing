4f2ec113256ed1603c34f900bf1f78164d886395
Chinh Tran <tran.chinh2808@gmail.com>
2023-04-03 09:29:49 -0400

    Add future work ideas for pixel class

--- pixel.h
+++ pixel.h
@@ -4,7 +4,15 @@
 #endif
 #ifndef PIXEL_H
 #define PIXEL_H
-
+/*
+Ideas for expansion of this class:
+1. A method for converting the color values from RGB (Red, Green, Blue) to another color space, such as HSL (Hue, Saturation, Lightness) or CMYK (Cyan, Magenta, Yellow, Key).
+2. A method for calculating the grayscale value of the pixel based on its RGB values.
+3. A method for calculating the brightness or luminance value of the pixel based on its RGB values.
+4. A method for performing operations on the color values, such as adding or subtracting a certain amount from each value, multiplying or dividing the values by a scalar, or blending the color with another color.
+5. A method for comparing two pixels to determine if they are the same or different.
+6. A method for printing the color values of the pixel to the console or to a file in a specific format.
+*/
 class Pixel{
     private:
         int red {0};
